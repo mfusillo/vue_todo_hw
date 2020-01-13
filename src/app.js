995c0904c70f9f600c199d1fd720f20954ea3166
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-13 15:58:12 +0000

    add view for listing items

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Dom Has Loaded');
-})
+  new Vue({
+    el: "#app",
+    data: {
+      thingsToDo: [
+        {name: "Clean Bathroom", isHighPriority: false},
+        {name: "Buy Groceries", isHighPriority: false},
+        {name: "Conquer World", isHighPriority: true}
+      ],
+      newThingToDo: {name: "", isHighPriority: false}
+    }
+  });
+});
